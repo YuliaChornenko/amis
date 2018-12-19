45658d67500f505bc491aceb6965583f6507fdc0
Yulia Chornenko <44753645+YuliaChornenko@users.noreply.github.com>
2018-12-19 15:50:06 +0200

    Update workshop6(1).py

--- km-83/Chornenko_Julia/workshop6/workshop6(1).py
+++ km-83/Chornenko_Julia/workshop6/workshop6(1).py
@@ -2,18 +2,22 @@ import re
 import plotly
 import plotly.graph_objs as go
 from plotly import tools
-nan=float('nan')
+from collections import Counter
 
-input_file='../filereader/vgsales.csv'
+nan = float('nan')
+
+input_file = '../filereader/vgsales.csv'
+
 
 def getYear(line):
-    result=re.split(r',', line, maxsplit=1)
-    Year=re.findall(r'[1-3][0-9]{3}',result[0])
+    result = re.split(r',', line, maxsplit=1)
+    Year = re.findall(r'[1-3][0-9]{3}', result[0])
     return Year[0], result[1]
+
 
 def getPlatform(line):
     result = re.split(r',', line, maxsplit=1)
-    Platform=result[0].strip()
+    Platform = result[0].strip()
     return Platform, result[1]
 
 
@@ -21,23 +25,25 @@ def getName(line):
     result = re.split(r',', line, maxsplit=1)
     return result[0], result[1]
 
+
 def getRank(line):
     result = re.split(r',', line, maxsplit=1)
     return result[0], result[1]
 
-current_line=0
+
+current_line = 0
 i = 0
 try:
 
     with open(input_file, encoding="utf8", mode='r') as file:
         file.readline()
-        line_number=1
-        dataset={}
+        line_number = 1
+        dataset = {}
         for line in file:
-            i+=1
+            i += 1
 
-            columns=line.split(',')
-            
+            columns = line.split(',')
+
             Rank, line = getRank(line)
             Name, line = getName(line)
             Platform, line = getPlatform(line)
@@ -46,25 +52,25 @@ try:
             except IndexError:
                 Year = 'Unknown'
             if Year not in list(dataset.keys()):
-                dataset[Year]={}
-                    
+                dataset[Year] = {}
+
             if Platform not in list(dataset[Year].keys()):
-                dataset[Year][Platform]=dict()
+                dataset[Year][Platform] = dict()
 
             if Name not in dataset[Year][Platform]:
                 dataset[Year][Platform][Name] = Rank
-#           print(dataset)
+    #           print(dataset)
     print(dataset)
 except IOError:
     print('Error with file', IOError.errno, IOError.strerror)
 except ValueError:
     print('Error in line', current_line, ValueError)
 
-input_file='../filereader/vgsales.csv'
+input_file = '../filereader/vgsales.csv'
 with open(input_file, encoding="utf8", mode='r') as file:
     file.readline()
 
-    count_of_game=dict()
+    count_of_game = dict()
 
     for line in file:
         columns = line.split(',')
@@ -72,43 +78,76 @@ with open(input_file, encoding="utf8", mode='r') as file:
         Year = columns[3]
 
         if Year not in count_of_game:
-            count_of_game[Year]=list()
+            count_of_game[Year] = list()
         if Name not in count_of_game[Year]:
             count_of_game[Year].append(Name)
     print(count_of_game)
 
-v=-1
-count=[]
-while v!=541:
-    v+=1
+v = -1
+count = []
+while v != 541:
+    v += 1
     count.append(len((count_of_game[Year])[v]))
 
-
-
-
+print(list(count_of_game.keys()))
 print(count)
-graph=[go.Scatter(x=list(count_of_game.keys()),
-             y=count,
-             name='Year - count of game')]
-
-plotly.offline.plot(graph,filename="1.html")
-
-
+scat = go.Scatter(x=list(count_of_game.keys()),
+                  y=count,
+                  name='Year - count of game')
 
 with open(input_file, encoding="utf8", mode='r') as file:
     file.readline()
 
-    platforms=list()
+    platforms = list()
 
     for line in file:
         columns = line.split(',')
         Platform = columns[2]
 
-
-        if Platform not in platforms:
-            platforms.append(Platform)
+        platforms.append(Platform)
     print(platforms)
 
+platforms1 = Counter(platforms)
+print(platforms1)
+
+pie = go.Pie(labels=list(platforms1.keys()),
+             values=list(platforms1.values()))
+
+with open(input_file, encoding="utf8", mode='r') as file:
+    file.readline()
+
+    rank = dict()
+
+    for line in file:
+        columns = line.split(',')
+
+        Rank = columns[0]
+        Platform = columns[2]
+
+        if Platform not in rank:
+            rank[Platform] = set()
+
+        rank[Platform].add(float(Rank))
+    print(rank)
+
+max_rank = list()
+for Platform in rank:
+    maximum = max(rank[Platform])
+    max_rank.append(maximum)
+print(max_rank)
+
+bar = go.Bar(x=list(rank.keys()),
+             y=max_rank)
+
+fig = tools.make_subplots(rows=2, cols=2)
+
+fig.append_trace(bar, 1, 1)
+
+fig.append_trace(pie, 1, 2)
+
+fig.append_trace(scat, 2, 1)
+
+plotly.offline.plot(fig, filename="myplotly.html")
 
 
 
